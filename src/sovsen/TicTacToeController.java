aeddf4b5b0c7d5dbbccb974d115e831948ed51a3
niel905g <niel905g@edu.easj.dk>
2018-09-20 10:10:14 +0200

    buttonClickHandler(ActionEvent evt) løst!

--- src/sovsen/TicTacToeController.java
+++ src/sovsen/TicTacToeController.java
@@ -13,17 +13,39 @@ public class TicTacToeController {
 
     @FXML
     Button b1;
-    @FXML Button b2;
-    @FXML Button b3;
-    @FXML Button b4;
-    @FXML Button b5;
-    @FXML Button b6;
-    @FXML Button b7;
-    @FXML Button b8;
-    @FXML Button b9;
+    @FXML
+    Button b2;
+    @FXML
+    Button b3;
+    @FXML
+    Button b4;
+    @FXML
+    Button b5;
+    @FXML
+    Button b6;
+    @FXML
+    Button b7;
+    @FXML
+    Button b8;
+    @FXML
+    Button b9;
 
     @FXML
     GridPane gameBoard;
 
+    private boolean PLAYER_X = true;
 
+    public void buttonClickHandler(ActionEvent evt) {
+
+        Button clickedButton = (Button) evt.getTarget();
+        String buttonLabel = clickedButton.getText();
+
+        if ("".equals(buttonLabel) && PLAYER_X) {
+            clickedButton.setText("X");
+            PLAYER_X = false;
+        } else if ("".equals(buttonLabel) && !PLAYER_X) {
+            clickedButton.setText("O");
+            PLAYER_X = true;
+        }
+    }
 }
